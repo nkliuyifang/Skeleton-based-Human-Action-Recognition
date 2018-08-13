5d3de40ca231433e6cf3517d7c5ef863aa1a980b
nkliuyifang <41351688+nkliuyifang@users.noreply.github.com>
2018-08-13 10:30:28 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,35 @@
 import os
 
-os.system("mkdir NTU_img_type_4/model")
-os.system("python train.py 'NTU_img_type_4' 'NTU_img_type_4/model' 192 0.01 60")
-os.system("python test.py 'NTU_img_type_4' 'NTU_img_type_4/model' 16")
+os.system("mkdir Northwestern-UCLA_img_type_4/model")
+os.system("python train.py 'Northwestern-UCLA_img_type_4' 'Northwestern-UCLA_img_type_4/model' 16 0.01 10")
+os.system("python test.py 'Northwestern-UCLA_img_type_4' 'Northwestern-UCLA_img_type_4/model' 16")
 
-os.system("mkdir NTU_img_type_3/model")
-os.system("python train.py 'NTU_img_type_3' 'NTU_img_type_3/model' 192 0.01 60")
-os.system("python test.py 'NTU_img_type_3' 'NTU_img_type_3/model' 16")
+os.system("mkdir Northwestern-UCLA_img_type_3/model")
+os.system("python train.py 'Northwestern-UCLA_img_type_3' 'Northwestern-UCLA_img_type_3/model' 16 0.01 10")
+os.system("python test.py 'Northwestern-UCLA_img_type_3' 'Northwestern-UCLA_img_type_3/model' 16")
 
-os.system("mkdir NTU_img_type_2/model")
-os.system("python train.py 'NTU_img_type_2' 'NTU_img_type_2/model' 192 0.01 60")
-os.system("python test.py 'NTU_img_type_2' 'NTU_img_type_2/model' 16")
+os.system("mkdir Northwestern-UCLA_img_type_2/model")
+os.system("python train.py 'Northwestern-UCLA_img_type_2' 'Northwestern-UCLA_img_type_2/model' 16 0.01 10")
+os.system("python test.py 'Northwestern-UCLA_img_type_2' 'Northwestern-UCLA_img_type_2/model' 16")
 
-os.system("mkdir NTU_img_type_1/model")
-os.system("python train.py 'NTU_img_type_1' 'NTU_img_type_1/model' 192 0.01 60")
-os.system("python test.py 'NTU_img_type_1' 'NTU_img_type_1/model' 16")
+os.system("mkdir Northwestern-UCLA_img_type_1/model")
+os.system("python train.py 'Northwestern-UCLA_img_type_1' 'Northwestern-UCLA_img_type_1/model' 16 0.01 10")
+os.system("python test.py 'Northwestern-UCLA_img_type_1' 'Northwestern-UCLA_img_type_1/model' 16")
+
+
+
+#os.system("mkdir NTU_img_type_4/model")
+#os.system("python train.py 'NTU_img_type_4' 'NTU_img_type_4/model' 192 0.01 60")
+#os.system("python test.py 'NTU_img_type_4' 'NTU_img_type_4/model' 16")
+#
+#os.system("mkdir NTU_img_type_3/model")
+#os.system("python train.py 'NTU_img_type_3' 'NTU_img_type_3/model' 192 0.01 60")
+#os.system("python test.py 'NTU_img_type_3' 'NTU_img_type_3/model' 16")
+#
+#os.system("mkdir NTU_img_type_2/model")
+#os.system("python train.py 'NTU_img_type_2' 'NTU_img_type_2/model' 192 0.01 60")
+#os.system("python test.py 'NTU_img_type_2' 'NTU_img_type_2/model' 16")
+#
+#os.system("mkdir NTU_img_type_1/model")
+#os.system("python train.py 'NTU_img_type_1' 'NTU_img_type_1/model' 192 0.01 60")
+#os.system("python test.py 'NTU_img_type_1' 'NTU_img_type_1/model' 16")
